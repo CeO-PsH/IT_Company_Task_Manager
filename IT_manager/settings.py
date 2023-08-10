034555c97126d8917a2c0e077489ba6cc586cf46
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-08-10 22:28:12 +0300

    made new it_mange_app in main directoty

--- IT_manager/settings.py
+++ IT_manager/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "it_manager_service",
+
 ]
 
 MIDDLEWARE = [
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "it_manager_app"
 ]
 
 ROOT_URLCONF = "IT_manager.urls"

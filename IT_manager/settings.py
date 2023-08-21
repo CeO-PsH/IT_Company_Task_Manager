180c380e83c9e2d96437161e47237b1c0cf25fe4
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-08-21 17:18:08 +0300

    change settings

--- IT_manager/settings.py
+++ IT_manager/settings.py
@@ -25,11 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "RENDER" not in os.environ
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://it-company-task-manager-c3x6.onrender.com"]
-
-
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     # ...

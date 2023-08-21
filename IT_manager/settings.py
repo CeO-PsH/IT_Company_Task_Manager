497c8d48a3fb67fbc5fbed0cfc347950231237e5
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-08-21 17:04:47 +0300

    Add allowed url

--- IT_manager/settings.py
+++ IT_manager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "https://it-company-task-manager-c3x6.onrender.com"]
 
 
 
